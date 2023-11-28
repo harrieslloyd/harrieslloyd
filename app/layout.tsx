db91cb166747bedcc50bf74fd8ad3f811b4ddb63
Harries Lloyd <harrieslloyd9@gmail.com>
2023-11-27 23:38:53 -0700

    testing dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
 							{children}
 					</div>
